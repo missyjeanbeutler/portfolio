7aee71828b9df526e17cc8cf071382f8c4b90387
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-04-25 16:06:53 -0600

    adding a slider instead

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $('.beaten-path').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+  });
+});
+
+$(document).ready(function () {
+  $('.dev-tracker').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+  });
+});
+
